b9c74a3b44c32b0229e529dd4a5be7ae410d5c7d
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2025-12-17 20:34:28 +0100

    .

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const router = require('./routes/approutes.js')
 const app = express()
 
 app.use(cors({
-    origin: "https://task-flow-steel-three.vercel.app/",
+    origin: "https://task-flow-steel-three.vercel.app",
   credentials: true
 
 }))
