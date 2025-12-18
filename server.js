484d3991bb9c868cc694f90c38c1b8064bcc0456
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2025-12-18 04:55:05 +0100

    ....

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const router = require('./routes/approutes.js')
 const app = express()
 app.use(cookie())
 app.use(cors({
-    origin: "https://taskflow-two-neon.vercel.app/",
+    origin: "https://taskflow-two-neon.vercel.app",
   credentials: true
 
 }))
