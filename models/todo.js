a200d0abb8a33ce9b70f9f8fc35f3989a6119539
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2025-12-21 17:41:31 +0100

    all correctly done and working

--- models/todo.js
+++ models/todo.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose')
 
-const todoschema = new mongoose.Schema({
-    description :{
-        type : String,
-        requried :true
+const todoschema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true
     },
-    title :{
-        type : String,
-        required : true 
+    title: {
+      type: String,
+      required: true
     },
     userId: {
-    type: String,  
-    required: true
+      type: String,
+      required: true
+    }
   },
+  { timestamps: true } 
+)
 
-})
-const cerateingtodo = mongoose.model('todo',todoschema)
+const cerateingtodo = mongoose.model('Todo', todoschema)
 module.exports = cerateingtodo
